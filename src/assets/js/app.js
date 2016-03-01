d73b95fa510fc920457820f3763ea8ed4ad00d40
Horacio Ibrahim <horacioibrahim@gmail.com>
2016-02-29 22:16:07 -0300

    Adaptado para backend producao

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -12,7 +12,7 @@
  */
 
 var siscomando = siscomando || {};
-siscomando.REMOTE_ADDR = 'http://localhost:5000'// 'https://agile-lake-26676.herokuapp.com';
+siscomando.REMOTE_ADDR = 'https://agile-lake-26676.herokuapp.com'// 'https://agile-lake-26676.herokuapp.com';
 siscomando.SESSION_KEY = 'scdata' // hint: In the sc-login this is sessionKey property.
 siscomando.currentUser = null;
 
@@ -26,5 +26,18 @@ siscomando.url = {
   followservices: siscomando.REMOTE_ADDR + '/api/follow/services'
 };
 
+/** `getParameterByName` simple way get simple params in the querystring.
+* This code is copied from:
+* http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+*/
+siscomando.getParameterByName = function (name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
 //'https://gist.githubusercontent.com/horacioibrahim/55ce6b7b8a338ab2ad59/raw/09aa4c7af173f00b93010357ec2c808ab7d90647/data_home.json'
