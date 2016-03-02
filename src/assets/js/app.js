37334fd9bc064ff8fa52e3ece8f55ba656c736d3
Horacio Ibrahim <horacioibrahim@gmail.com>
2016-03-02 17:48:49 -0300

    Image to empty screens

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -12,18 +12,18 @@
  */
 
 var siscomando = siscomando || {};
-siscomando.REMOTE_ADDR = 'https://agile-lake-26676.herokuapp.com'// 'https://agile-lake-26676.herokuapp.com';
+siscomando.REMOTE_ADDR = 'http://localhost:5000'// 'https://agile-lake-26676.herokuapp.com';
 siscomando.SESSION_KEY = 'scdata' // hint: In the sc-login this is sessionKey property.
 siscomando.currentUser = null;
 
 /* Siscomando URL's */
 siscomando.url = {
   home: '#/home',
-  channels: siscomando.REMOTE_ADDR + '/api/services',
+  channels: siscomando.REMOTE_ADDR + '/api/services/followed',
   feeds: siscomando.REMOTE_ADDR + '/api/feeds',
   login: siscomando.REMOTE_ADDR + '/loginexp',
-  searchservices: siscomando.REMOTE_ADDR + '/api/search/services/?q=',
-  followservices: siscomando.REMOTE_ADDR + '/api/follow/services'
+  searchservices: siscomando.REMOTE_ADDR + '/api/services/search/?q=',
+  followservices: siscomando.REMOTE_ADDR + '/api/services/follow'
 };
 
 /** `getParameterByName` simple way get simple params in the querystring.
