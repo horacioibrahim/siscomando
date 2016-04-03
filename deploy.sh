f05c33030abb9c4cf4aa420e1d3b414abd14cb36
Horacio Ibrahim <horacioibrahim@gmail.com>
2016-04-03 10:32:05 -0300

    Improved deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@
 # comment/uncomment:
 #WEINER='<script src=\"http:\/\/debug-software.intel.com\/target\/target-script-min.js#y2xTv4rXcg8e2Bx0Ht1OszIiSqmpJUxdmW_GcyVPcps\"><\/script><\/body>'
 
-CORDOVA='<script src=\"cordova.js\"><\/script><\/body>'
+#CORDOVA='<script src=\"cordova.js\"><\/script><\/body>'
     
 if [ ! -z "$WEINER" ]
     then
@@ -23,9 +23,9 @@ vulcanize --strip-comments --inline-css src/index.html > scapp/www/index.html
 #sed -i.bak "s/<\/body>/$CORDOVA/g" scapp/www/index.html
 
 # Copy .js and images
-cp -r src/assets/* scapp/www/assets/
+cp -r src/assets scapp/www/assets
 # cp -r src/assets/img/* scapp/www/img/
-cp -r src/elements/* scapp/www/elements/
+cp -r src/elements scapp/www/elements
 
 # Return original HTML
 if [ ! -z "$WEINER" ]
@@ -37,8 +37,8 @@ fi
 cd $DIRECTORY_PROJECT 
 cd scapp
 
-#cordova prepare
-#cordova build android
+cordova prepare
+cordova build android
 # if android device connected will work fine!
 #cordova run android
 
